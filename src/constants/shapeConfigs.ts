3237bc2e293d25a3e1ade2759be659580daa7439
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-07-10 12:55:46 -0300

    proporções

--- src/constants/shapeConfigs.ts
+++ src/constants/shapeConfigs.ts
@@ -4,21 +4,21 @@ export const shapeConfigs: ShapeConfig[] = [
   {
     type: 'cylinder',
     displayName: 'Cilindro',
-    defaultDimensions: { diameter: 200, height: 200 }
+    defaultDimensions: { diameter: 1500, height: 2000 }
   },
   {
     type: 'cone',
     displayName: 'Cone',
-    defaultDimensions: { diameter: 200, height: 200 }
+    defaultDimensions: { diameter: 1500, height: 2000 }
   },
   {
     type: 'cube',
     displayName: 'Cubo',
-    defaultDimensions: { width: 200, height: 200, depth: 200 }
+    defaultDimensions: { width: 1500, height: 1500, depth: 1500 }
   },
   {
     type: 'conicReduction',
     displayName: 'Redução Cônica',
-    defaultDimensions: { topDiameter: 300, bottomDiameter: 200, height: 200 }
+    defaultDimensions: { topDiameter: 1000, bottomDiameter: 500, height: 1000 }
   }
 ];
